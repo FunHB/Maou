4d1a1a455e3df67e55e9ede55af37320b784399e
Mateusz <71179061+Sygnator@users.noreply.github.com>
2020-12-17 19:14:48 +0100

    Fix art cmd v2

--- handlers/command.handler.js
+++ handlers/command.handler.js
@@ -34,16 +34,17 @@ module.exports = (client) => {
 
         // Check channel commands and ignore report command
 
-        if (msg.content.startsWith(prefix+"art")) {
-            if (msg.channel.id !== "723107790393966594") {
-                if (author.id !== owner) {
-                    return msg.reply(`Poleceń możesz używać na kanele <#723107790393966594>!`); }
-            }
-        }
+        // if (msg.content.startsWith(prefix+"art")) {
+        //     if (msg.channel.id !== "723107790393966594") {
+        //         if (author.id !== owner) {
+        //             return msg.reply(`Poleceń możesz używać na kanele <#723107790393966594>!`); }
+        //     }
+        // }
 
         // if (msg.content.startsWith(prefix+"art") && msg.channel.id !== "723107790393966594" && author.id !== owner) return msg.reply(`Poleceń możesz używać na kanele <#723107790393966594>!`);
 
         if (msg.content.startsWith(prefix+"report") || msg.content.startsWith(prefix+"raport") || msg.content.startsWith(prefix+"zglos") || msg.content.startsWith(prefix+"zgłoś") || msg.content.startsWith(prefix+"zgloś") || msg.content.startsWith(prefix+"zgłoś")) {} else {
+            if (msg.content.startsWith(prefix+"art") && msg.channel.id !== "723107790393966594" && author.id !== owner) return msg.reply(`Poleceń możesz używać na kanele <#723107790393966594>!`);
             if (msg.channel.id !== botCommandsChannel && author.id !== owner && msg.content !== prefix) return msg.reply(`Poleceń możesz używać na kanele <#${botCommandsChannel}>!`);
         }
         
