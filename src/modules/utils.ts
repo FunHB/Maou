e99ef759383df8ebdff4ac3b6306eb867754aedb
FunHB <marcel.krolczyk@gmail.com>
2021-02-23 17:12:17 +0100

    date display correction

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -1,6 +1,6 @@
 export class Utils {
     public static dateToString(date: Date, noSeconds = false): string {
-        const out = `${('0' + date.getUTCDate()).slice(-2)}.${('0' + date.getUTCMonth() + 1).slice(-2)}.${date.getUTCFullYear()} ${('0' + date.getUTCHours() + 1).slice(-2)}:${('0' + date.getUTCMinutes()).slice(-2)}`
+        const out = `${('0' + date.getUTCDate()).slice(-2)}.${('0' + (date.getUTCMonth() + 1)).slice(-2)}.${date.getUTCFullYear()} ${('0' + (date.getUTCHours() + 1)).slice(-2)}:${('0' + date.getUTCMinutes()).slice(-2)}`
         if (noSeconds) return out
         return `${out}:${('0' + date.getUTCSeconds()).slice(-2)}`
     }
