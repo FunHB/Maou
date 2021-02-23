6dbb188c59fcc0601af652121d22c873febb8828
FunHB <marcel.krolczyk@gmail.com>
2021-02-23 19:40:26 +0100

    reported message link corrected

--- src/commands/report.ts
+++ src/commands/report.ts
@@ -1,6 +1,7 @@
-import { Message, MessageEmbed } from 'discord.js';
+import { Channel, Guild, Message, MessageEmbed } from 'discord.js';
 import { channelType, Colors, Command } from '../api';
 import { Config } from '../config';
+import { Utils } from '../modules/utils';
 const config = new Config()
 
 export class ReportCommand implements Command {
@@ -14,7 +15,7 @@ export class ReportCommand implements Command {
     public cooldown = 1
 
     public async execute(message: Message, args: string[]): Promise<void> {
-        const { channel } = message
+        const { channel, author, guild } = message
         const reportChannel = message.guild.channels.cache.get(config.reportsChannel);
         const reported = args.shift()
         const reason = args.join(' ')
@@ -35,7 +36,7 @@ export class ReportCommand implements Command {
             return
         }
 
-        if (!args[1]) {
+        if (!args[0]) {
             await channel.send(new MessageEmbed({
                 color: Colors.Error,
                 description: 'Musisz podać powód zgłoszenia!'
@@ -44,7 +45,7 @@ export class ReportCommand implements Command {
         }
 
         if (message.deletable) {
-            await message.delete();
+            await message.delete()
         }
 
         await channel.send(new MessageEmbed({
@@ -52,21 +53,24 @@ export class ReportCommand implements Command {
             description: 'Wiadomość została zgłoszona!'
         }))
 
-
+        // link to repair - report from other channel
         if (reportChannel.isText()) {
             await reportChannel.send(new MessageEmbed({
                 color: Colors.Info,
                 description: 'Report',
                 fields: [
-                    { name: 'Zgłoszone przez:', value: `Użytkownik: <@!${message.author.id}> ID: ${message.author.id}` },
+                    { name: 'Zgłoszone przez:', value: `Użytkownik: <@!${author.id}> ID: ${author.id}` },
                     { name: 'Zgłoszono na kanale:', value: `<#${channel.id}>` },
                     { name: 'Id zgłoszonej wiadmości:', value: reported },
-                    { name: 'Link do zgłoszonej wiadomości:', value: `https://discord.com/channels/${message.guild.id}/${channel}/${reported}` },
-                    { name: 'Czas:', value: message.createdAt },
+                    { name: 'Link do zgłoszonej wiadomości:', value: `https://discord.com/channels/${guild.id}/${this.getChannel(guild, reported)}/${reported}` },
+                    { name: 'Czas:', value: Utils.dateToString(message.createdAt) },
                     { name: 'Powód:', value: reason }
                 ]
             }));
         }
+    }
 
+    private getChannel(guild: Guild, channelID: string): Channel {
+        return guild.channels.cache.get(channelID)
     }
 }
