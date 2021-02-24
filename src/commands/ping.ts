62fd2c245cf8ecc37f8b86999d7e566dbd108e94
FunHB <marcel.krolczyk@gmail.com>
2021-02-24 13:01:12 +0100

    ping repair

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -11,7 +11,8 @@ export class PingCommand implements Command {
 
     public async execute(message: Message): Promise<void> {
         const pingMessage = await message.channel.send('Ping?')
-        await pingMessage.edit(new MessageEmbed({
+        await pingMessage.delete()
+        await message.channel.send(new MessageEmbed({
             color: Colors.Success,
             description: `Ping **<@!${message.member.id}>** wynosi - ${pingMessage.createdTimestamp - message.createdTimestamp}ms`
         }))
