23d5e2872383d9fdcf5c294d378c1b03e24ff42f
FunHB <marcel.krolczyk@gmail.com>
2021-04-08 16:59:44 +0200

    changenick command

---
+++ src/mod_commands/changenick.ts
@@ -0,0 +1,29 @@
+import { Message, MessageEmbed } from 'discord.js'
+import { channelType, Colors, Command } from '../api'
+import { Config } from '../config'
+import { Utils } from '../modules/utils'
+
+export class ChangeNickCommand implements Command {
+    public name = 'changenick'
+    public description = 'Zmienia pseudonim podanemu użytkownikowi'
+    public aliases: string[] = ['zmieńnick', 'zmiennick', 'zmienpseudonim', 'zmieńpseudonim']
+    public args = true
+    public roles: string[] = [Config.modRole]
+    public usage = '<użytkownik> [nowy pseudonim]'
+    public channelType: channelType = channelType.normal
+    public guildonly = true
+
+    public async execute(message: Message, args: string[]): Promise<void> {
+        const member = await Utils.getMember(message, args.shift())
+        const oldNickname = member.nickname
+
+        if (!member) return
+
+        await member.setNickname(args.join(' '))
+
+        await message.channel.send(new MessageEmbed({
+            color: Colors.Success,
+            description: `Pseudonim użytkownika <@${member.id}> został zmieniony z ${oldNickname} na ${member.nickname}`
+        }))
+    }
+}
