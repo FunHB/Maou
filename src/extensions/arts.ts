e519006bad8146e75a97fe3db0975d9d57441719
yuukinoe <awjpjg@gmail.com>
2022-02-28 00:01:01 +0100

    arts api url fix

--- src/extensions/arts.ts
+++ src/extensions/arts.ts
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 export class Arts {
-    private static url = 'https://api.waifu.im/sfw/waifu'
+    private static url = 'https://api.waifu.im/random/?selected_tags=waifu&is_nsfw=no'
 
     public static async getRandomImage(): Promise<string> {
         const response = await fetch(this.url)
